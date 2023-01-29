d4c6dde51677b42fe89f10b780603b5b8801f246
ishuki916 <89925767+ishuki916@users.noreply.github.com>
2023-01-29 12:17:14 +0800

    create project

---
+++ src/main/java/com/muwei/demo/BookmycampsiteApplication.java
@@ -0,0 +1,13 @@
+package com.muwei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmycampsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmycampsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muwei/demo/BookmycampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muwei.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmycampsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
